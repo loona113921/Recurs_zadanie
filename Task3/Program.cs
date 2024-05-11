d55b9788a73cd9f7b7194165e7da4c57d76ba678
Yuliya Ustimenko <loona113921@gmail.com>
2024-05-11 22:01:03 +0300

    Добавила решение Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void Main()
+{
+    int[] array = { 2, 6, 9, 5, 6 };
+
+    ReverseArray(array, array.Length - 1);
+}
+
+void ReverseArray(int[] arr, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(arr[index] + " ");
+        ReverseArray(arr, index - 1);
+    }
+}
+Main();
